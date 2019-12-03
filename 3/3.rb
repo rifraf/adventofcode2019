29014b2f4e2339c65e326ffa3d36dc6f9500428e
David Lake <rifraf@rifraf.net>
2019-12-03 12:23:56 +0000

    6 start

--- 3/3.rb
+++ 3/3.rb
@@ -72,3 +72,92 @@ path2 = 'L998,U308,R889,D471,R719,U326,L6,U802,L608,U149,R454,U6,R837,U255,L720,
 
 result = min_distance(matching_points(path1, path2))
 puts "Part 1 result = #{result}"
+
+#===========================================================
+# Test driver2
+#===========================================================
+class Examples2 < Test::Unit::TestCase
+
+  def test_example1
+    p1 = %w{R8 U5 L5 D3}
+    p2 = %w{U7 R6 D4 L4}
+    matches = matching_points(p1, p2)
+    assert_equal [[6,5], [3, 3]], matches
+    assert_equal 20, path_length_to(p1, [3, 3])
+    assert_equal 15, path_length_to(p1, [6, 5])
+    assert_equal 20, path_length_to(p2, [3, 3])
+    assert_equal 15, path_length_to(p2, [6, 5])
+  end
+
+  def test_example2
+    p1 = %w{R75 D30 R83 U83 L12 D49 R71 U7 L72}
+    p2 = %w{U62 R66 U55 R34 D71 R55 D58 R83}
+    matches = matching_points(p1, p2)
+    assert_equal [[158, -12], [146, 46], [155, 4], [155, 11]], matches
+    assert_equal 159, min_distance(matches)
+
+    result = matches.map do |m|
+      path_length_to(p1, m) + path_length_to(p2, m)
+    end.min
+    assert_equal 610, result
+  end
+
+  def test_example3
+    p1 = %w{R98 U47 R26 D63 R33 U87 L62 D20 R33 U53 R51}
+    p2 = %w{U98 R91 D20 R16 D67 R40 U7 R15 U6 R7}
+    matches = matching_points(p1, p2)
+    assert_equal [[107, 47], [124, 11], [157, 18], [107, 71], [107, 51]],  matches
+    assert_equal 135, min_distance(matches)
+
+    result = matches.map do |m|
+      path_length_to(p1, m) + path_length_to(p2, m)
+    end.min
+    assert_equal 410, result
+  end
+end
+
+#===========================================================
+def path_length_to(path, point)
+  path_length = 0
+  x = y = 0
+  lengths = []
+  path.each do |step|
+    break unless step =~ /(\S)(\d+)/
+    direction = $1
+    length = $2.to_i
+    case direction
+    when 'R'
+      length.times do |l|
+        path_length += 1
+        lengths << path_length and break if [x + l + 1, y] == point
+      end
+      x += length
+    when 'L'
+      length.times do |l|
+        path_length += 1
+        lengths << path_length and break if [x - l - 1, y] == point
+      end
+      x -= length
+    when 'U'
+      length.times do |l|
+        path_length += 1
+        lengths << path_length and break if [x, y + l + 1] == point
+      end
+      y += length
+    when 'D'
+      length.times do |l|
+        path_length += 1
+        lengths << path_length and break if [x, y - l - 1] == point
+      end
+      y -= length
+    end
+  end
+  lengths.min
+end
+
+#===========================================================
+matches = matching_points(path1, path2)
+result = matches.map do |m|
+  path_length_to(path1, m) + path_length_to(path2, m)
+end.min
+puts "Part 2 result = #{result}" # <- 4158
