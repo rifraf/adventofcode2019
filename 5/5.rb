284eec5140e506cd346bc9164783256a87c3de97
David Lake <rifraf@rifraf.net>
2019-12-05 13:28:08 +0000

    Day 5 completed

--- 5/5.rb
+++ 5/5.rb
@@ -60,14 +60,48 @@ def run_intcode(input, output, *program)
       prod = read_param(1, program, index, param1_mode) * read_param(2, program, index, param2_mode)
       write_param(3, program, index, param3_mode, prod)
       index += 4
-    when 3
+    when 3 # Store input
       value = input.shift
-      program[program [index + 1]] = value
+      write_param(1, program, index, param1_mode, value)
       index += 2
-    when 4
-      value = program[program [index + 1]]
+    when 4 # output first parameter
+      value = read_param(1, program, index, param1_mode)
       output << value
       index += 2
+    when 5 # is jump-if-true
+      # if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+      value = read_param(1, program, index, param1_mode)
+      if value.zero?
+        index += 3
+      else
+        index = read_param(2, program, index, param2_mode)
+      end
+    when 6 # is jump-if-false
+      # if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+      value = read_param(1, program, index, param1_mode)
+      if value.zero?
+        index = read_param(2, program, index, param2_mode)
+      else
+        index += 3
+      end
+    when 7 # less than
+      # if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+      value = if read_param(1, program, index, param1_mode) < read_param(2, program, index, param2_mode)
+        1
+      else
+        0
+      end
+      write_param(3, program, index, param3_mode, value)
+      index += 4
+    when 8 # equals
+      # if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+      value = if read_param(1, program, index, param1_mode) == read_param(2, program, index, param2_mode)
+        1
+      else
+        0
+      end
+      write_param(3, program, index, param3_mode, value)
+      index += 4
     else
       puts
       puts "Invalid OP #{program[index]} at #{index}"
@@ -132,6 +166,112 @@ class Examples5a < Test::Unit::TestCase
     input  = [1]
     output = []
     run_intcode(input, output, *prog)
-    assert_equal [3, 0, 0, 0, 0, 0, 0, 0, 0, 14_522_484], output # <--- correct answer
+    assert_equal [0, 0, 0, 0, 0, 0, 0, 0, 0, 14_522_484], output # <--- correct answer
+  end
+end
+
+#===========================================================
+# Test driver5b
+#===========================================================
+class Examples5b < Test::Unit::TestCase
+
+  def test_input1
+    prog = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+
+    output = []
+    run_intcode([8], output, *prog)
+    assert_equal [1], output
+
+    output = []
+    run_intcode([9], output, *prog)
+    assert_equal [0], output
+  end
+
+  def test_input2
+    prog = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+
+    output = []
+    run_intcode([7], output, *prog)
+    assert_equal [1], output
+
+    output = []
+    run_intcode([8], output, *prog)
+    assert_equal [0], output
+  end
+
+  def test_input3
+    prog = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
+
+    output = []
+    run_intcode([8], output, *prog)
+    assert_equal [1], output
+
+    output = []
+    run_intcode([9], output, *prog)
+    assert_equal [0], output
+
+    output = []
+    run_intcode([7], output, *prog)
+    assert_equal [0], output
+  end
+
+  def test_input4
+    prog = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+
+    output = []
+    run_intcode([7], output, *prog)
+    assert_equal [1], output
+
+    output = []
+    run_intcode([8], output, *prog)
+    assert_equal [0], output
+  end
+
+  def test_input5
+    prog = [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]
+
+    output = []
+    run_intcode([0], output, *prog)
+    assert_equal [0], output
+
+    output = []
+    run_intcode([100], output, *prog)
+    assert_equal [1], output
+  end
+
+  def test_input6
+    prog = [3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1]
+
+    output = []
+    run_intcode([0], output, *prog)
+    assert_equal [0], output
+
+    output = []
+    run_intcode([100], output, *prog)
+    assert_equal [1], output
+  end
+
+  def test_input7
+    prog = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
+
+    output = []
+    run_intcode([7], output, *prog)
+    assert_equal [999], output
+
+    output = []
+    run_intcode([8], output, *prog)
+    assert_equal [1000], output
+
+    output = []
+    run_intcode([9], output, *prog)
+    assert_equal [1001], output
+  end
+
+  def test_part2
+    prog = [3,225,1,225,6,6,1100,1,238,225,104,0,1101,65,73,225,1101,37,7,225,1101,42,58,225,1102,62,44,224,101,-2728,224,224,4,224,102,8,223,223,101,6,224,224,1,223,224,223,1,69,126,224,101,-92,224,224,4,224,1002,223,8,223,101,7,224,224,1,223,224,223,1102,41,84,225,1001,22,92,224,101,-150,224,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1101,80,65,225,1101,32,13,224,101,-45,224,224,4,224,102,8,223,223,101,1,224,224,1,224,223,223,1101,21,18,225,1102,5,51,225,2,17,14,224,1001,224,-2701,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,101,68,95,224,101,-148,224,224,4,224,1002,223,8,223,101,1,224,224,1,223,224,223,1102,12,22,225,102,58,173,224,1001,224,-696,224,4,224,1002,223,8,223,1001,224,6,224,1,223,224,223,1002,121,62,224,1001,224,-1302,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1008,226,677,224,102,2,223,223,1005,224,329,1001,223,1,223,7,677,226,224,102,2,223,223,1006,224,344,1001,223,1,223,1007,226,677,224,1002,223,2,223,1006,224,359,1001,223,1,223,1007,677,677,224,102,2,223,223,1005,224,374,1001,223,1,223,108,677,677,224,102,2,223,223,1006,224,389,101,1,223,223,8,226,677,224,102,2,223,223,1005,224,404,101,1,223,223,7,226,677,224,1002,223,2,223,1005,224,419,101,1,223,223,8,677,226,224,1002,223,2,223,1005,224,434,101,1,223,223,107,677,677,224,1002,223,2,223,1006,224,449,101,1,223,223,7,677,677,224,1002,223,2,223,1006,224,464,101,1,223,223,1107,226,226,224,102,2,223,223,1006,224,479,1001,223,1,223,1007,226,226,224,102,2,223,223,1006,224,494,101,1,223,223,108,226,677,224,1002,223,2,223,1006,224,509,101,1,223,223,1108,226,677,224,102,2,223,223,1006,224,524,1001,223,1,223,1008,226,226,224,1002,223,2,223,1005,224,539,101,1,223,223,107,226,226,224,102,2,223,223,1006,224,554,101,1,223,223,8,677,677,224,102,2,223,223,1005,224,569,101,1,223,223,107,226,677,224,102,2,223,223,1005,224,584,101,1,223,223,1108,226,226,224,1002,223,2,223,1005,224,599,1001,223,1,223,1008,677,677,224,1002,223,2,223,1005,224,614,101,1,223,223,1107,226,677,224,102,2,223,223,1005,224,629,101,1,223,223,1108,677,226,224,1002,223,2,223,1005,224,644,1001,223,1,223,1107,677,226,224,1002,223,2,223,1006,224,659,1001,223,1,223,108,226,226,224,102,2,223,223,1006,224,674,101,1,223,223,4,223,99,226]
+    input  = [5]
+    output = []
+    run_intcode(input, output, *prog)
+    assert_equal [4655956], output # <--- correct answer
   end
 end
