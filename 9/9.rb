e00faad343f5bb843af409bf95e57294dda0365f
David Lake <rifraf@rifraf.net>
2019-12-09 20:18:51 +0000

    day 9 part 1

---
+++ 9/9.rb
@@ -0,0 +1,49 @@
+#
+# Advent of Code 2019 day 9
+#
+require 'pp'
+require 'test/unit'
+require_relative '../intcode/intcode'
+require_relative '../7/7'
+
+#===========================================================
+# Test driver9a
+#===========================================================
+class Examples9a < Test::Unit::TestCase
+  def test_1
+    program = [109,2000,109,19,204,-34,99]
+    program[1985] = 1985
+    output = []
+    run_intcode([], output, *program)
+    assert_equal 1985, output[0]
+  end
+
+  def test_2
+    program = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99].freeze
+    output = []
+    run_intcode([], output, *program)
+    assert_equal output, program
+  end
+
+  def test_3
+    program = [1102,34915192,34915192,7,4,7,99,0].freeze
+    output = []
+    run_intcode([], output, *program)
+    assert_equal 1219070632396864, output[0]
+  end
+
+  def test_4
+    program = [104,1125899906842624,99].freeze
+    output = []
+    run_intcode([], output, *program)
+    assert_equal 1125899906842624, output[0]
+  end
+
+  def test_a
+    program = IO.read('input1.txt').split(',').map(&:to_i)
+    input = [1]
+    output = []
+    run_intcode(input, output, *program)
+    assert_equal 3765554916, output[0] # <- correct
+  end
+end
--- intcode/intcode.rb
+++ intcode/intcode.rb
@@ -3,6 +3,7 @@
 #===========================================================
 def run_intcode(input, output, *program)
   index = 0
+  sda_base = 0
   while (program[index] % 100) != 99
     instruction = program[index]
     opcode = instruction % 100
@@ -10,77 +11,94 @@ def run_intcode(input, output, *program)
     param2_mode = (instruction / 1_000) % 10
     param3_mode = (instruction / 10_000) % 10
 
+    # p [index, opcode, param1_mode, param2_mode, param3_mode, sda_base]
+
     case opcode
-    when 1
-      sum = read_param(1, program, index, param1_mode) + read_param(2, program, index, param2_mode)
-      write_param(3, program, index, param3_mode, sum)
+    when 1 # +
+      sum = read_param(1, program, index, param1_mode, sda_base) + read_param(2, program, index, param2_mode, sda_base)
+      write_param(3, program, index, param3_mode, sda_base, sum)
       index += 4
-    when 2
-      prod = read_param(1, program, index, param1_mode) * read_param(2, program, index, param2_mode)
-      write_param(3, program, index, param3_mode, prod)
+    when 2 # *
+      prod = read_param(1, program, index, param1_mode, sda_base) * read_param(2, program, index, param2_mode, sda_base)
+      write_param(3, program, index, param3_mode, sda_base, prod)
       index += 4
     when 3 # Store input
       value = input.shift
-      write_param(1, program, index, param1_mode, value)
+      write_param(1, program, index, param1_mode, sda_base, value)
       index += 2
     when 4 # output first parameter
-      value = read_param(1, program, index, param1_mode)
+      value = read_param(1, program, index, param1_mode, sda_base)
       output << value
       index += 2
     when 5 # is jump-if-true
       # if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-      value = read_param(1, program, index, param1_mode)
+      value = read_param(1, program, index, param1_mode, sda_base)
       if value.zero?
         index += 3
       else
-        index = read_param(2, program, index, param2_mode)
+        index = read_param(2, program, index, param2_mode, sda_base)
       end
     when 6 # is jump-if-false
       # if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-      value = read_param(1, program, index, param1_mode)
+      value = read_param(1, program, index, param1_mode, sda_base)
       if value.zero?
-        index = read_param(2, program, index, param2_mode)
+        index = read_param(2, program, index, param2_mode, sda_base)
       else
         index += 3
       end
     when 7 # less than
       # if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-      value = if read_param(1, program, index, param1_mode) < read_param(2, program, index, param2_mode)
+      value = if read_param(1, program, index, param1_mode, sda_base) < read_param(2, program, index, param2_mode, sda_base)
                 1
               else
                 0
               end
-      write_param(3, program, index, param3_mode, value)
+      write_param(3, program, index, param3_mode, sda_base, value)
       index += 4
     when 8 # equals
       # if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-      value = if read_param(1, program, index, param1_mode) == read_param(2, program, index, param2_mode)
+      value = if read_param(1, program, index, param1_mode, sda_base) == read_param(2, program, index, param2_mode, sda_base)
                 1
               else
                 0
               end
-      write_param(3, program, index, param3_mode, value)
+      write_param(3, program, index, param3_mode, sda_base, value)
       index += 4
+    when 9 # adjusts the relative base
+      value = read_param(1, program, index, param1_mode, sda_base)
+      sda_base += value
+      index += 2
     else
       puts
       puts "Invalid OP #{program[index]} at #{index}"
       exit(1)
     end
   end
+  # p [index, opcode, param1_mode, param2_mode, param3_mode, sda_base]
+
   program
 end
 
 #===========================================================
-def read_param(num, program, index, mode)
+def read_param(num, program, index, mode, base)
   case mode
   when 0
-    program[program[index + num]]
+    program[program[index + num]] || 0
   when 1
-    program[index + num]
+    program[index + num] || 0
+  when 2
+    program[base + program[index + num]] || 0
   end
 end
 
 #===========================================================
-def write_param(num, program, index, _mode, value)
-  program[program[index + num]] = value
+def write_param(num, program, index, mode, base, value)
+  case mode
+  when 0
+    program[program[index + num]] = value
+  when 1
+    program[index + num] = value
+  when 2
+    program[base + program[index + num]] = value
+  end
 end
