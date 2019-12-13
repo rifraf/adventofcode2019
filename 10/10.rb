e18ba15723256bf59e2ead576bf2f92cd6f05039
David Lake <rifraf@rifraf.net>
2019-12-13 17:32:33 +0000

    day 10 part 1

--- 10/10.rb
+++ 10/10.rb
@@ -57,28 +57,125 @@ class Examples10 < Test::Unit::TestCase
     asteroids = asteroid_locations.map { |xy| Asteroid.new(xy) }
 
     # Find vector to each asteroid
+    location_and_visibility = []
     asteroids.each do |asteroid|
-      pp [asteroid.location, asteroid.vectors_to(asteroid_locations)]
+      other_asteroids = asteroid_locations.reject { |loc| loc == asteroid.location }
+      angles = asteroid.polar_vectors_to(other_asteroids).map { |_distance, angle| angle }
+      visible_asteroids = angles.uniq.count # If angle matches then only the first will be seen.
+      # visible_asteroids = asteroid.visible_asteroids(other_asteroids)
+      assert_equal visible_asteroids, asteroid.visible_asteroids(other_asteroids)
+      assert_equal expected_visibility_coords[asteroid.location], visible_asteroids.to_s
+      location_and_visibility << [asteroid.location, visible_asteroids]
     end
+
+    best_location = location_and_visibility.max { |a, b| a[1] <=> b[1] }[0]
+    assert_equal [3, 4], best_location
+  end
+
+  def test_example2
+    asteroid_map = ['......#.#.', '#..#.#....', '..#######.', '.#.#.###..', '.#..#.....', '..#....#.#', '#..#....#.', '.##.#..###', '##...#..#.', '.#....####']
+    # Best is 5,8 with 33 other asteroids detected:
+    assert_equal [[5, 8], 33], best_monitoring_location(asteroid_map)
+  end
+
+  def test_example3
+    asteroid_map = ['#.#...#.#.', '.###....#.', '.#....#...', '##.#.#.#.#', '....#.#.#.', '.##..###.#', '..#...##..', '..##....##', '......#...', '.####.###.']
+    # Best is 1,2 with 35 other asteroids detected:
+    assert_equal [[1, 2], 35], best_monitoring_location(asteroid_map)
+  end
+
+  def test_example4
+    asteroid_map = ['.#..#..###', '####.###.#', '....###.#.', '..###.##.#', '##.##.#.#.', '....###..#', '..#.#..#.#', '#..#.#.###', '.##...##.#', '.....#.#..']
+    # Best is 6,3 with 41 other asteroids detected
+    assert_equal [[6, 3], 41], best_monitoring_location(asteroid_map)
+  end
+
+  def test_example5
+    asteroid_map = [
+      '.#..##.###...#######',
+      '##.############..##.',
+      '.#.######.########.#',
+      '.###.#######.####.#.',
+      '#####.##.#.##.###.##',
+      '..#####..#.#########',
+      '####################',
+      '#.####....###.#.#.##',
+      '##.#################',
+      '#####.##.###..####..',
+      '..######..##.#######',
+      '####.##.####...##..#',
+      '.#####..#.######.###',
+      '##...#.##########...',
+      '#.##########.#######',
+      '.####.#.###.###.#.##',
+      '....##.##.###..#####',
+      '.#.#.###########.###',
+      '#.#.#.#####.####.###',
+      '###.##.####.##.#..##'
+    ]
+    # Best is 11,13 with 210 other asteroids detected
+    assert_equal [[11, 13], 210], best_monitoring_location(asteroid_map)
+  end
+
+  def test_part1
+    asteroid_map = [
+      '#.#.##..#.###...##.#....##....###',
+      '...#..#.#.##.....#..##.#...###..#',
+      '####...#..#.##...#.##..####..#.#.',
+      '..#.#..#...#..####.##....#..####.',
+      '....##...#.##...#.#.#...#.#..##..',
+      '.#....#.##.#.##......#..#..#..#..',
+      '.#.......#.....#.....#...###.....',
+      '#.#.#.##..#.#...###.#.###....#..#',
+      '#.#..........##..###.......#...##',
+      '#.#.........##...##.#.##..####..#',
+      '###.#..#####...#..#.#...#..#.#...',
+      '.##.#.##.........####.#.#...##...',
+      '..##...#..###.....#.#...#.#..#.##',
+      '.#...#.....#....##...##...###...#',
+      '###...#..#....#............#.....',
+      '.#####.#......#.......#.#.##..#.#',
+      '#.#......#.#.#.#.......##..##..##',
+      '.#.##...##..#..##...##...##.....#',
+      '#.#...#.#.#.#.#..#...#...##...#.#',
+      '##.#..#....#..##.#.#....#.##...##',
+      '...###.#.#.......#.#..#..#...#.##',
+      '.....##......#.....#..###.....##.',
+      '........##..#.#........##.......#',
+      '#.##.##...##..###.#....#....###.#',
+      '..##.##....##.#..#.##..#.....#...',
+      '.#.#....##..###.#...##.#.#.#..#..',
+      '..#..##.##.#.##....#...#.........',
+      '#...#.#.#....#.......#.#...#..#.#',
+      '...###.##.#...#..#...##...##....#',
+      '...#..#.#.#..#####...#.#...####.#',
+      '##.#...#..##..#..###.#..........#',
+      '..........#..##..#..###...#..#...',
+      '.#.##...#....##.....#.#...##...##'
+    ]
+    assert_equal [[22, 28], 326], best_monitoring_location(asteroid_map) # <- correct
   end
 end
 
 #===========================================================
 class Asteroid
-
   attr_reader :location
 
   def initialize(location)
     @location = location
   end
 
-  def vectors_to(locations)
+  def polar_vectors_to(locations)
     locations.map do |location|
       route_x = location[0] - @location[0]
       route_y = location[1] - @location[1]
-      distance = Math.sqrt((route_x * route_x) + (route_y * route_y))
-      [ distance, Float(route_x) / Float(route_y) ]
+      [Math.hypot(route_y, route_x), Math.atan2(route_y, route_x).round(8)] # Distance, angle to 8 dp
     end
+  end
+
+  def visible_asteroids(locations)
+    angles = polar_vectors_to(locations).map { |_distance, angle| angle }
+    visible_asteroids = angles.uniq.count # If angle matches then only the first will be seen.
   end
 end
 
@@ -93,4 +190,21 @@ def map_by_coordinates(string_map)
     y += 1
   end
   map
+end
+
+#===========================================================
+def best_monitoring_location(asteroid_map)
+  # Create asteroid directory
+  asteroid_locations = map_by_coordinates(asteroid_map).keys
+  asteroids = asteroid_locations.map { |xy| Asteroid.new(xy) }
+
+  # Find vector to each asteroid
+  location_and_visibility = []
+  asteroids.each do |asteroid|
+    other_asteroids = asteroid_locations.reject { |loc| loc == asteroid.location }
+    visible_asteroids = asteroid.visible_asteroids(other_asteroids)
+    location_and_visibility << [asteroid.location, visible_asteroids]
+  end
+
+  best_location = location_and_visibility.max { |a, b| a[1] <=> b[1] }
 end
