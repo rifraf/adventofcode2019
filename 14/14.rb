26151166607f853fd15dcc7182875052f949f217
David Lake <rifraf@rifraf.net>
2019-12-17 17:46:21 +0000

    Still struggling

--- 14/14.rb
+++ 14/14.rb
@@ -20,12 +20,20 @@ class Examples14 < Test::Unit::TestCase
 
     cookbook = CookBook.new
     cookbook.add_recipes reactions
-    pp cookbook
+    # pp cookbook
 
-    assert_equal 10, cookbook.element('A').input_quantity(1, 'ORE')
-    assert_equal 10, cookbook.element('A').input_quantity(9, 'ORE')
-    assert_equal 10, cookbook.element('A').input_quantity(10, 'ORE')
-    assert_equal 20, cookbook.element('A').input_quantity(11, 'ORE')
+    assert_equal 1, cookbook.element('A').max_reactions
+    assert_equal 1, cookbook.element('B').max_reactions
+    assert_equal 2, cookbook.element('C').max_reactions
+    assert_equal 3, cookbook.element('D').max_reactions
+    assert_equal 4, cookbook.element('E').max_reactions
+    assert_equal 5, cookbook.element('FUEL').max_reactions
+
+    pp cookbook.element('FUEL').calculate_ore_quantity
+    # assert_equal 10, cookbook.element('A').input_quantity(1, 'ORE')
+    # assert_equal 10, cookbook.element('A').input_quantity(9, 'ORE')
+    # assert_equal 10, cookbook.element('A').input_quantity(10, 'ORE')
+    # assert_equal 20, cookbook.element('A').input_quantity(11, 'ORE')
 
     # assert_equal 1, cookbook.element('B').input_quantity('ORE')
     # assert_equal 11, cookbook.element('C').input_quantity('ORE')
@@ -37,7 +45,7 @@ end
 class CookBook
   def initialize
     @elements = {
-      'ORE' => Element.new('ORE', 1, self)
+      'ORE' => Element.new('ORE', 1)
     }
   end
 
@@ -47,43 +55,71 @@ class CookBook
 
   def add_recipes(recipes)
     recipes.each { |r| add_recipe r }
+    @elements.each { |_k, e| e.resolve_precursor @elements }
   end
 
   def add_recipe(recipe)
     source, dest = recipe.split("=>")
-    if dest =~ /(\d+)\s+(\S+)/
-      name = $2
-      min_qty = $1.to_i
-      @elements[name] = Element.new(name, min_qty, self)
-      @elements[name].built_from(source)
-    end
+    return unless dest =~ /(\d+)\s+(\S+)/
+
+    min_qty = $1.to_i
+    name = $2
+    @elements[name] = Element.new(name, min_qty)
+    @elements[name].built_from(source)
   end
 end
 
 #===========================================================
 class Element
-  def initialize(name, min_qty, cookbook)
+
+  attr_reader :building_blocks
+
+  def initialize(name, min_qty)
     @name = name
     @min_qty = min_qty
-    @cookbook = cookbook
     @built_from = []
+    @building_blocks = {}
   end
 
   def built_from(source)
     @built_from = source.split(',').map do |src|
       src =~ /(\d+)\s+(\S+)/
-      [@min_qty, $1.to_i, $2]
+      [$1.to_i, $2]
     end
   end
 
-  def input_quantity(required, base_element)
-    return min_qty if @name == base_element
-    total = 0
-    # required =
-    # @built_from.each do |qty, el|
-    #   total += required * @cookbook.element(el).input_quantity(qty, base_element)
-    # end
-    total
+  def resolve_precursor(element_map)
+    @built_from.each do |qty, name|
+      @building_blocks[element_map[name]] = qty
+    end
+  end
+
+  def max_reactions
+    return 0 if @building_blocks.empty?
+    @max_reactions ||= @building_blocks.keys.map { |el| 1 + el.max_reactions }.max
+  end
+
+  # def input_quantity(required, base_element)
+  #   return min_qty if @name == base_element
+  #   total = 0
+  #   # required =
+  #   # @built_from.each do |qty, el|
+  #   #   total += required * @cookbook.element(el).input_quantity(qty, base_element)
+  #   # end
+  #   total
+  # end
+
+  def calculate_ore_quantity
+    new_blocks = {}
+    @building_blocks.each do |el, min_qty|
+      if el.max_reactions == 1
+        new_blocks[el] = qty
+      else
+        el.building_blocks.each do |next_el, next_qty|
+          new_blocks[next_el] = next_qty
+        end
+      end
+    end
   end
 
 end
