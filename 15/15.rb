559c4d34d2df1e72a21f039cd8540f82ab375220
David Lake <rifraf@rifraf.net>
2019-12-21 10:21:53 +0000

    New verions of map trace

--- 15/15.rb
+++ 15/15.rb
@@ -4,7 +4,6 @@
 require 'pp'
 require 'test/unit'
 require_relative '../intcode/intcode'
-require_relative '../7/7'
 
 #===========================================================
 # Test driver15a
@@ -12,7 +11,7 @@ require_relative '../7/7'
 class Examples15a < Test::Unit::TestCase
 
   #=========================================================
-  def test_simple_movement
+  def xtest_simple_movement
     map = MapOfShip.new
     navigator = DummyNavigator.new([Navigator::WALL, Navigator::WALL, Navigator::WALL, Navigator::MOVED, Navigator::FOUND_OXYGEN, Navigator::WALL])
     droid = Droid.new(navigator)
@@ -79,6 +78,8 @@ class Examples15a < Test::Unit::TestCase
 
     # ----------------------------------------
     # Work out walls with random walk
+    # No good for the second part because it
+    # doesn't explore the complete space
     # ----------------------------------------
     # loop do
     #   response, location = droid.move([Navigator::NORTH, Navigator::SOUTH, Navigator::EAST, Navigator::WEST].sample)
@@ -163,7 +164,7 @@ class MapOfShip
     x_coords = cells.keys.map { |x, _y| x }.sort.uniq
     y_coords = cells.keys.map { |_x, y| y }.sort.uniq
     puts
-    (y_coords[0]..y_coords[-1]).each do |y|
+    (y_coords[0]..y_coords[-1]).reverse_each do |y|
       (x_coords[0]..x_coords[-1]).each do |x|
         print cells[[x, y]] || '.'
       end
@@ -179,7 +180,7 @@ class MapOfShip
     @colored.each { |loc, val| cells[loc] = '~' }
     puts
     freeze_dimensions
-    @y_range.each do |y|
+    @y_range.reverse_each do |y|
       @x_range.each do |x|
         print cells[[x, y]] || '.'
       end
@@ -217,6 +218,29 @@ class DummyNavigator
   # --------------------------------------------------------
   def response_to_move(_move)
     @responses.shift
+  end
+end
+
+#===========================================================
+class FakeNavigator
+  # --------------------------------------------------------
+  def initialize(walls)
+    @walls = walls
+    @x = 0
+    @y = 0
+  end
+
+  # --------------------------------------------------------
+  def response_to_move(move)
+    next_pos = [@x + 1, @y] if move == Navigator::EAST
+    next_pos = [@x - 1, @y] if move == Navigator::WEST
+    next_pos = [@x, @y + 1] if move == Navigator::NORTH
+    next_pos = [@x, @y - 1] if move == Navigator::SOUTH
+    return Navigator::WALL if @walls.include?(next_pos)
+
+    @x = next_pos[0]
+    @y = next_pos[1]
+    Navigator::MOVED
   end
 end
 
@@ -273,7 +297,6 @@ class Droid
                     when Navigator::EAST
                       [@position[0] + 1, @position[1]]
                     end
-
     response = @navigator.response_to_move(step)
     @position = next_position unless response == Navigator::WALL
     [response, next_position]
@@ -285,38 +308,129 @@ end
 #===========================================================
 class Examples15b < Test::Unit::TestCase
 
-  #=========================================================
-  def test_diffusion
+  # Try search and backtrack.
+  #  Go straight until hit wall, keeping track of route
+  #   Record l & r as a branch point
+  #    Remove l if it is a known wall
+  #    Remove r if it is a known wall
+  #    If branch point is empty, backtrack to last nonempty branch point
+  #     Finished if backtracks to 0,0
+  #    Else
+  #     Go in direction of first entry in branch point
+  #
+  # Try only going left when hitting a wall and stop when back at first hit
+  # Should work if the map is enclosed (else oxygen will leak). May miss walls
+  # not connected to others by > 1 grid
 
+  #=========================================================
+  def xtest_simple_search
+    map = MapOfShip.new
+    navigator = FakeNavigator.new([[0,1], [-1,1], [-1,0], [-1,-1], [0,-1], [1,-1], [1,0],  [1,1] ])
+    droid = Droid.new(navigator)
+
+    # Initial
+    assert_equal [], map.oxygen_location
+    assert_equal [], map.walls
+    assert_equal [0, 0], droid.position
+
+    direction = Navigator::NORTH
+    6.times do
+      response, location = droid.move(direction)
+      break if location == map.walls.first
+      if response ==  Navigator::WALL
+        map.add_wall location
+        # Go left
+        direction = case direction
+                    when Navigator::NORTH then Navigator::WEST
+                    when Navigator::SOUTH then Navigator::EAST
+                    when Navigator::WEST  then Navigator::SOUTH
+                    when Navigator::EAST  then Navigator::NORTH
+                    end
+      end
+      map.visualize_with_droid droid.position
+    end
+
+  end
+
+  #=========================================================
+  def test_exploring_part2
     program = IO.read('input1.txt').split(',').map(&:to_i)
     map = MapOfShip.new
-    # navigator = Navigator.new(program)
-    # droid = Droid.new(navigator)
+    navigator = Navigator.new(program)
+    droid = Droid.new(navigator)
+    location = [0, 0]
+
+    direction = Navigator::NORTH
+    loop do
+      response, location = droid.move(direction)
+      if response ==  Navigator::WALL
+        map.add_wall location
+        # Go left
+        direction = case direction
+                    when Navigator::NORTH then Navigator::WEST
+                    when Navigator::SOUTH then Navigator::EAST
+                    when Navigator::WEST  then Navigator::SOUTH
+                    when Navigator::EAST  then Navigator::NORTH
+                    end
+      else
+        map.add_oxygen location if Navigator::FOUND_OXYGEN == response
+
+        # Go right
+        direction = case direction
+                    when Navigator::NORTH then Navigator::EAST
+                    when Navigator::SOUTH then Navigator::WEST
+                    when Navigator::WEST  then Navigator::NORTH
+                    when Navigator::EAST  then Navigator::SOUTH
+                    end
+      end
+      break if (droid.position == [0, 0]) && (direction == Navigator::NORTH)
+    end
+
+
+    map.visualize_with_droid droid.position
+
+    # Save result
+    IO.write('walls2', map.walls.to_s)
+    IO.write('oxygen2', map.oxygen_location.to_s)
 
     # ----------------------------------------
     # Read map back from file
     # ----------------------------------------
-    eval(IO.read('oxygen')).each { |loc| map.add_oxygen loc }
-    eval(IO.read('walls')).each { |loc| map.add_wall loc }
-    map.freeze_dimensions
+    # eval(IO.read('oxygen')).each { |loc| map.add_oxygen loc }
+    # eval(IO.read('walls')).each { |loc| map.add_wall loc }
 
-    # ----------------------------------------
-    # Colorize a few steps
-    # ----------------------------------------
-    minutes = 0
-    seekers = map.oxygen_location
-    map.color seekers, minutes
-
-    while seekers.any? do
-      minutes += 1
-      seekers = map.paint_adjacent_to(seekers, minutes)
-      p seekers, minutes
-    end
-
-    puts "Diffuses in #{minutes}"
-    map.visualize_colorized
-    # seekers = map.oxygen_location
-    # p seekers
-    # 249 too low
+    # # Answer is 248 (by hand - see 'map')
+    # map.visualize_with_droid droid.position
   end
+
+  #=========================================================
+  # def xtest_diffusion
+  #   map = MapOfShip.new
+
+  #   # ----------------------------------------
+  #   # Read map back from file
+  #   # ----------------------------------------
+  #   eval(IO.read('oxygen')).each { |loc| map.add_oxygen loc }
+  #   eval(IO.read('walls')).each { |loc| map.add_wall loc }
+  #   map.freeze_dimensions
+
+  #   # ----------------------------------------
+  #   # Colorize a few steps
+  #   # ----------------------------------------
+  #   minutes = 0
+  #   seekers = map.oxygen_location
+  #   map.color seekers, minutes
+
+  #   while seekers.any? do
+  #     minutes += 1
+  #     seekers = map.paint_adjacent_to(seekers, minutes)
+  #     p seekers, minutes
+  #   end
+
+  #   puts "Diffuses in #{minutes}"
+  #   map.visualize_colorized
+  #   # seekers = map.oxygen_location
+  #   # p seekers
+  #   # 249 too low
+  # end
 end
