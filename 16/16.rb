84079bceba8b5d5a37872c3569539a46af3f04b2
David Lake <rifraf@rifraf.net>
2019-12-24 09:26:49 +0000

    Part 2 completed

--- 16/16.rb
+++ 16/16.rb
@@ -138,54 +138,80 @@ end
 class Examples16b < Test::Unit::TestCase
 
   #=========================================================
-  def test_extending_signal
-    seed = [0, 1, 0, -1]
-    signal = '12345678'.each_char.map(&:to_i)
-
+  def test_p2_ex1
+    # The input is 32 chars so becomes 320_000
+    # The sequence starts at           303_673 which is in the end zone
+    # On each iteration:
+    #  The last digit is N
+    #  N-1 is sum of N + N-1
+    #  N-2 is sum of N + N-2...
+    #  303_673 is sum of 303_673, 303_674, 303_675...
+    signal = '03036732577212944063491565474664'.each_char.map(&:to_i)
+    signal = (signal * 10_000)[303_673...-1]
     result = signal
-    patterns = fft_patterns(signal, seed)
     100.times do
-      result = fft_phase(result, patterns)
+      limit = result.length - 2
+      limit.downto(0) do |i|
+        result[i] = (result[i] + result[i + 1]).abs % 10
+      end
     end
-
-    p result
-    assert_equal [2, 3, 8, 4, 5, 6, 7, 8], result[0..7]
-
-    signal = signal * 2
-    result = signal
-    patterns = fft_patterns(signal, seed)
-    100.times do
-      result = fft_phase(result, patterns)
-    end
-    # p signal
-    # pp patterns
-    p result
-
-    signal = signal * 2
-    result = signal
-    patterns = fft_patterns(signal, seed)
-    100.times do
-      result = fft_phase(result, patterns)
-    end
-    # p signal
-    # pp patterns
-    p result
+    assert_equal [8, 4, 4, 6, 2, 0, 2, 6], result[0..7]
   end
 
-  # #=========================================================
-  # def test_part2
-  #   seed = [0, 1, 0, -1]
-  #   signal = '59775675999083203307460316227239534744196788252810996056267313158415747954523514450220630777434694464147859581700598049220155996171361500188470573584309935232530483361639265796594588423475377664322506657596419440442622029687655170723364080344399753761821561397734310612361082481766777063437812858875338922334089288117184890884363091417446200960308625363997089394409607215164553325263177638484872071167142885096660905078567883997320316971939560903959842723210017598426984179521683810628956529638813221927079630736290924180307474765551066444888559156901159193212333302170502387548724998221103376187508278234838899434485116047387731626309521488967864391'.each_char.map(&:to_i)
-  #   signal *= 10_000
+  #=========================================================
+  def test_p2_ex2
+    # The input is 32 chars so becomes 320_000
+    # The sequence starts at           293_510 which is in the end zone
+    # On each iteration:
+    #  The last digit is N
+    #  N-1 is sum of N + N-1
+    #  N-2 is sum of N + N-2...
+    signal = '02935109699940807407585447034323'.each_char.map(&:to_i)
+    signal = (signal * 10_000)[293_510...-1]
+    result = signal
+    100.times do
+      limit = result.length - 2
+      limit.downto(0) do |i|
+        result[i] = (result[i] + result[i + 1]).abs % 10
+      end
+    end
+    assert_equal [7, 8, 7, 2, 5, 2, 7, 0], result[0..7]
+  end
 
-  #   assert_equal 5977567, (signal[0] * 1_000_000) + (signal[1] * 100_000) + (signal[2] * 10_000) + (signal[3] * 1_000) + (signal[4] * 100) + (signal[5] * 10) + (signal[6] * 1)
-  #   result = signal
-  #   patterns = fft_patterns(signal, seed)
-  #   1.times do
-  #     result = fft_phase(result, patterns)
-  #   end
-  #   p result
-  #   # assert_equal [2, 4, 1, 7, 6, 1, 7, 6], result[0..7]
-  # end
+  #=========================================================
+  def test_p2_ex3
+    # The input is 32 chars so becomes 320_000
+    # The sequence starts at           308_177 which is in the end zone
+    # On each iteration:
+    #  The last digit is N
+    #  N-1 is sum of N + N-1
+    #  N-2 is sum of N + N-2...
+    signal = '03081770884921959731165446850517'.each_char.map(&:to_i)
+    signal = (signal * 10_000)[308_177...-1]
+    result = signal
+    100.times do
+      limit = result.length - 2
+      limit.downto(0) do |i|
+        result[i] = (result[i] + result[i + 1]).abs % 10
+      end
+    end
+    assert_equal [5, 3, 5, 5, 3, 7, 3, 1], result[0..7]
+  end
+
+  #=========================================================
+  def test_part2
+    # The input has 650 chars, so will become 6_500_000 long
+    # The sequence we want starts at          5_977_567 which is in the end zone
+    signal = '59775675999083203307460316227239534744196788252810996056267313158415747954523514450220630777434694464147859581700598049220155996171361500188470573584309935232530483361639265796594588423475377664322506657596419440442622029687655170723364080344399753761821561397734310612361082481766777063437812858875338922334089288117184890884363091417446200960308625363997089394409607215164553325263177638484872071167142885096660905078567883997320316971939560903959842723210017598426984179521683810628956529638813221927079630736290924180307474765551066444888559156901159193212333302170502387548724998221103376187508278234838899434485116047387731626309521488967864391'.each_char.map(&:to_i)
+    signal = (signal * 10_000)[5_977_567...-1]
+    result = signal
+    100.times do
+      limit = result.length - 2
+      limit.downto(0) do |i|
+        result[i] = (result[i] + result[i + 1]).abs % 10
+      end
+    end
+    assert_equal [5, 3, 2, 0, 1, 6, 0, 2], result[0..7] # 53201602 <- correct
+  end
 
 end
