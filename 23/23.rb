d002fd69079858727bf7505ad40d0f923c280f1c
David Lake <rifraf@rifraf.net>
2020-01-12 17:46:31 +0000

    day 23 being worked on

---
+++ 23/23.rb
@@ -0,0 +1,81 @@
+#
+# Advent of Code 2023 day 23
+#
+require 'pp'
+require 'test/unit'
+require_relative '../intcode/intcode'
+
+#===========================================================
+# Test driver23a
+#===========================================================
+class Examples23a < Test::Unit::TestCase
+  #=========================================================
+  def test_nonblocking_input
+    program = IO.read('input1.txt').split(',').map(&:to_i)
+
+    send_requests = Queue.new # Collects all requests for messages to be sent
+
+    test_thread = nil
+    input = NonBlockingInputStream.new
+    output = MessageRequestReceiver.new do |dest, x, y|
+      send_requests << [dest, x, y]
+      test_thread.kill if dest == 37 # Need to be able to kill intcode
+    end
+
+    # Node 0 will start by sending messages to 6 nodes (some of which are the same)
+    input << 0
+    test_thread = Thread.new { run_intcode(input, output, *program.dup) }
+    test_thread.join
+
+    assert_equal [39, 76651, 28154], send_requests.pop
+    assert_equal [26, 42571, 28154], send_requests.pop
+    assert_equal [26, 85142, 28154], send_requests.pop
+    assert_equal [37, 96589, 28154], send_requests.pop
+  end
+end
+
+#===========================================================
+# Returns -1 if no data
+#===========================================================
+class NonBlockingInputStream
+  def initialize
+    @q = Queue.new
+  end
+
+  def shift
+    ret = @q.empty? ? -1 : @q.shift
+    ret
+  end
+
+  def <<(val)
+    @q << val
+  end
+end
+
+#===========================================================
+# Expects data as [dest, X, Y]. Yields it to creator
+#===========================================================
+class MessageRequestReceiver
+  def initialize(&blk)
+    @q = Queue.new
+    @report = blk
+  end
+
+  def shift
+    @q.shift
+  end
+
+  def <<(val)
+    @q << val
+    @report.call(@q.pop, @q.pop, @q.pop) if @q.size == 3
+  end
+end
+
+#===========================================================
+# Test driver23b
+#===========================================================
+class Examples23b < Test::Unit::TestCase
+  #=========================================================
+  def test_part2
+  end
+end
