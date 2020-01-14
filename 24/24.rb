41cc51d2c8c624e8d7bf52d62cbaf4792b494404
David Lake <rifraf@rifraf.net>
2020-01-14 20:17:19 +0000

    day 24 part 2 needs adjacency rules

--- 24/24.rb
+++ 24/24.rb
@@ -39,22 +39,6 @@ class Examples24a < Test::Unit::TestCase
   end
 
   def test_biodiversity_value
-    # To understand the nature of the bugs, watch for the first time a layout of bugs
-    # and empty spaces matches any previous layout. In the example above, the first
-    # layout to appear twice is:
-    #
-    # .....
-    # .....
-    # .....
-    # #....
-    # .#...
-    #
-    # To calculate the biodiversity rating for this layout, consider each tile left-to-right
-    # in the top row, then left-to-right in the second row, and so on. Each of these
-    # tiles is worth biodiversity points equal to increasing powers of two: 1, 2, 4, 8, 16, 32,
-    # and so on. Add up the biodiversity points for tiles with bugs; in this example,
-    # the 16th tile (32768 points) and 22nd tile (2097152 points) have bugs,
-    # a total biodiversity rating of 2129920.
     example = [
       '.....',
       '.....',
@@ -87,28 +71,39 @@ end
 #===========================================================
 class Surface
   #=========================================================
-  def initialize(text_array)
+  def initialize(text_array = nil)
     @grid = []
-    5.times { @grid << [nil, nil, nil, nil, nil] }
+    5.times { @grid << [0, 0, 0, 0, 0] }
+    load_bugs(text_array) if text_array
+  end
+
+  #=========================================================
+  def load_bugs(text_array)
     text_array.each_with_index do |line, y|
       x = 0
       line.each_char do |c|
-        @grid[y][x] = 1 if c == '#'
+        @grid[y][x] = (c == '#') ? 1 : 0
         x += 1
       end
     end
   end
 
   #=========================================================
-  def evolve!
-    nextgen = []
+  def prepare_next_generation
+    @nextgen = []
     5.times do |y|
-      nextgen << []
+      @nextgen << []
       5.times do |x|
-        nextgen[y] << _next_gen(@grid, x, y)
+        @nextgen[y] << _next_gen(@grid, x, y)
       end
     end
-    @grid = nextgen
+    self
+  end
+
+  #=========================================================
+  def evolve!
+    prepare_next_generation
+    @grid = @nextgen
     self
   end
 
@@ -118,7 +113,7 @@ class Surface
     incr = 1
     @grid.each do |l|
       l.each do |v|
-        val += incr if v
+        val += incr unless v.zero?
         incr <<= 1
       end
     end
@@ -129,7 +124,7 @@ class Surface
   def to_a
     ret = []
     5.times do |y|
-      ret << @grid[y].map { |i| i.nil? ? '.' : '#' }.join
+      ret << @grid[y].map { |i| i.zero? ? '.' : '#' }.join
     end
     ret
   end
@@ -137,22 +132,108 @@ class Surface
   #=========================================================
   def _next_gen(grid, x, y)
     adj = _adjacent_bugs(grid, x, y)
-    if grid[y][x]
-      # Bug
-      (adj == 1) ? 1 : nil
-    else
+    if grid[y][x].zero?
       # Empty
-      ((adj == 1) || (adj == 2)) ? 1 : nil
+      ((adj == 1) || (adj == 2)) ? 1 : 0
+    else
+      # Bug
+      (adj == 1) ? 1 : 0
     end
   end
 
   #=========================================================
   def _adjacent_bugs(grid, x, y)
     adjacent_count = 0
-    adjacent_count += 1 if (y > 0) && grid[y - 1][x]
-    adjacent_count += 1 if (y < 4) && grid[y + 1][x]
-    adjacent_count += 1 if (x > 0) && grid[y][x - 1]
-    adjacent_count += 1 if (x < 4) && grid[y][x + 1]
+    adjacent_count += 1 if (y > 0) && !grid[y - 1][x].zero?
+    adjacent_count += 1 if (y < 4) && !grid[y + 1][x].zero?
+    adjacent_count += 1 if (x > 0) && !grid[y][x - 1].zero?
+    adjacent_count += 1 if (x < 4) && !grid[y][x + 1].zero?
+    adjacent_count
+  end
+end
+
+#===========================================================
+# Test driver24b
+#===========================================================
+class Examples24b < Test::Unit::TestCase
+  #=========================================================
+  def test_1_recursive_evolutions
+    initial = [
+      '....#',
+      '#..#.',
+      '#..##',
+      '..#..',
+      '#....'
+    ]
+    minute1 = [
+      '#..#.',
+      '####.',
+      '###.#',
+      '##.##',
+      '.##..'
+    ]
+    minute1_up = [
+      '.....',
+      '.....',
+      '.....',
+      '.....',
+      '.....'
+    ]
+    minute1_down = [
+      '.....',
+      '.....',
+      '.....',
+      '.....',
+      '.....'
+    ]
+
+    evolution_count = 1
+    surfaces_needed = 2 * evolution_count + 1
+    surfaces = []
+    surfaces_needed.times { surfaces << RecursiveSurface.new }
+    surfaces[evolution_count].load_bugs(initial)
+
+    empty = [
+      '.....',
+      '.....',
+      '.....',
+      '.....',
+      '.....'
+    ]
+    assert_equal empty,   surfaces[0].to_a
+    assert_equal initial, surfaces[1].to_a
+    assert_equal empty,   surfaces[2].to_a
+
+    surfaces[0].connect(nil, surfaces[1])
+    surfaces[1].connect(surfaces[0], surfaces[2])
+    surfaces[2].connect(surfaces[1], nil)
+
+    surfaces.each(&:prepare_next_generation)
+    surfaces.each(&:evolve!)
+
+    assert_equal minute1_down, surfaces[0].to_a
+    assert_equal minute1,      surfaces[1].to_a
+    assert_equal minute1_up,   surfaces[2].to_a
+  end
+end
+
+#===========================================================
+class RecursiveSurface < Surface
+  def connect(down, up)
+    @down = down
+    @up = up
+  end
+
+  #=========================================================
+  def _adjacent_bugs(grid, x, y)
+    puts "_adjacent_bugs(grid, #{x}, #{y}) / #{@down} / #{@up}"
+    adjacent_count = 0
+
+    # TODO
+    # adjacent_count += 1 if (y > 0) && !grid[y - 1][x].zero?
+    # adjacent_count += 1 if (y < 4) && !grid[y + 1][x].zero?
+    # adjacent_count += 1 if (x > 0) && !grid[y][x - 1].zero?
+    # adjacent_count += 1 if (x < 4) && !grid[y][x + 1].zero?
     adjacent_count
   end
 end
