10741da9cc29b04efe738475ca2dab7e0ecb347a
David Lake <rifraf@rifraf.net>
2020-01-15 20:59:04 +0000

    day 24 part 2 adjacency rules not quite there yet

--- 24/24.rb
+++ 24/24.rb
@@ -94,7 +94,7 @@ class Surface
     5.times do |y|
       @nextgen << []
       5.times do |x|
-        @nextgen[y] << _next_gen(@grid, x, y)
+        @nextgen[y] << _next_gen(x, y)
       end
     end
     self
@@ -130,9 +130,9 @@ class Surface
   end
 
   #=========================================================
-  def _next_gen(grid, x, y)
-    adj = _adjacent_bugs(grid, x, y)
-    if grid[y][x].zero?
+  def _next_gen(x, y)
+    adj = _adjacent_bugs(x, y)
+    if @grid[y][x].zero?
       # Empty
       ((adj == 1) || (adj == 2)) ? 1 : 0
     else
@@ -142,12 +142,12 @@ class Surface
   end
 
   #=========================================================
-  def _adjacent_bugs(grid, x, y)
+  def _adjacent_bugs(x, y)
     adjacent_count = 0
-    adjacent_count += 1 if (y > 0) && !grid[y - 1][x].zero?
-    adjacent_count += 1 if (y < 4) && !grid[y + 1][x].zero?
-    adjacent_count += 1 if (x > 0) && !grid[y][x - 1].zero?
-    adjacent_count += 1 if (x < 4) && !grid[y][x + 1].zero?
+    adjacent_count += 1 if (y > 0) && !@grid[y - 1][x].zero?
+    adjacent_count += 1 if (y < 4) && !@grid[y + 1][x].zero?
+    adjacent_count += 1 if (x > 0) && !@grid[y][x - 1].zero?
+    adjacent_count += 1 if (x < 4) && !@grid[y][x + 1].zero?
     adjacent_count
   end
 end
@@ -158,34 +158,11 @@ end
 class Examples24b < Test::Unit::TestCase
   #=========================================================
   def test_1_recursive_evolutions
-    initial = [
-      '....#',
-      '#..#.',
-      '#..##',
-      '..#..',
-      '#....'
-    ]
-    minute1 = [
-      '#..#.',
-      '####.',
-      '###.#',
-      '##.##',
-      '.##..'
-    ]
-    minute1_up = [
-      '.....',
-      '.....',
-      '.....',
-      '.....',
-      '.....'
-    ]
-    minute1_down = [
-      '.....',
-      '.....',
-      '.....',
-      '.....',
-      '.....'
-    ]
+    initial      = ['....#', '#..#.', '#..##', '..#..', '#....']
+    minute1      = ['#..#.', '####.', '##..#', '##.##', '.##..']
+    minute1_up   = ['.....', '..#..', '...#.', '..#..', '.....']
+    minute1_down = ['....#', '..#.#', '....#', '..#.#', '#####']
+    empty        = ['.....', '.....', '.....', '.....', '.....']
 
     evolution_count = 1
     surfaces_needed = 2 * evolution_count + 1
@@ -193,20 +170,15 @@ class Examples24b < Test::Unit::TestCase
     surfaces_needed.times { surfaces << RecursiveSurface.new }
     surfaces[evolution_count].load_bugs(initial)
 
-    empty = [
-      '.....',
-      '.....',
-      '.....',
-      '.....',
-      '.....'
-    ]
     assert_equal empty,   surfaces[0].to_a
     assert_equal initial, surfaces[1].to_a
     assert_equal empty,   surfaces[2].to_a
 
     surfaces[0].connect(nil, surfaces[1])
-    surfaces[1].connect(surfaces[0], surfaces[2])
-    surfaces[2].connect(surfaces[1], nil)
+    (surfaces_needed - 2).times do |i|
+      surfaces[i + 1].connect(surfaces[i + 0], surfaces[i + 2])
+    end
+    surfaces[surfaces_needed - 1].connect(surfaces[surfaces_needed - 2], nil)
 
     surfaces.each(&:prepare_next_generation)
     surfaces.each(&:evolve!)
@@ -215,25 +187,145 @@ class Examples24b < Test::Unit::TestCase
     assert_equal minute1,      surfaces[1].to_a
     assert_equal minute1_up,   surfaces[2].to_a
   end
+
+  #=========================================================
+  def test_10_recursive_evolutions
+    initial      = ['....#', '#..#.', '#..##', '..#..', '#....']
+
+    evolution_count = 10
+    surfaces_needed = 2 * evolution_count + 1
+    surfaces = []
+    surfaces_needed.times { surfaces << RecursiveSurface.new }
+    surfaces[evolution_count].load_bugs(initial)
+
+    surfaces[0].connect(nil, surfaces[1])
+    (surfaces_needed - 2).times do |i|
+      surfaces[i + 1].connect(surfaces[i + 0], surfaces[i + 2])
+    end
+    surfaces[surfaces_needed - 1].connect(surfaces[surfaces_needed - 2], nil)
+
+    10.times do
+      surfaces.each(&:prepare_next_generation)
+      surfaces.each(&:evolve!)
+    end
+
+    puts
+    surfaces.each_with_index do |s, i|
+      p [i - evolution_count, s.to_a]
+    end
+
+    # ['.....', '.....', '.....', '.....', '.....']
+    # ['.....', '.....', '.....', '.....', '.....']
+    # ['.....', '.....', '.....', '.....', '.....']
+    # ['.....', '.....', '.....', '.....', '.....']
+    # ['#####', '.....', '.....', '.....', '#####']
+    # ['#####', '.....', '#...#', '.....', '#####']
+    # ['##.##', '.#.#.', '#...#', '.#.#.', '##.##']
+    # ['#....', '....#', '#....', '....#', '#....']
+    # ['#.#..', '.....', '....#', '..#..', '..#..']
+    # ['.....', '.#.#.', '##.##', '#.#.#', '.#.#.']
+    # ['#...#', '.##..', '.....', '#....', '.##..']
+    # ['#####', '#...#', '...##', '#...#', '#####']
+    # ['..##.', '##..#', '....#', '##..#', '..##.']
+    # ['#####', '.#.#.', '....#', '.#.#.', '#####']
+    # ['#####', '.#...', '.#...', '.#...', '#####']
+    # ['.##..', '.#.#.', '.#...', '.#.#.', '.##..']
+    # ['..#.#', '...#.', '....#', '...#.', '..#.#']
+    # ['##.#.', '##..#', '#....', '##..#', '##.#.']
+    # ['#.#.#', '#....', '....#', '#....', '#.#.#']
+    # ['..#..', '.#.#.', '....#', '.#.#.', '..#..'] m5
+    # ['.....', '..#..', '...#.', '..#..', '.....']
+
+    depthm5 = ['..#..', '.#.#.', '....#', '.#.#.', '..#..']
+    depthm4 = ['...#.', '...##', '.....', '...##', '...#.']
+    depthm3 = ['#.#..', '.#...', '.....', '.#...', '#.#..']
+    depthm2 = ['.#.##', '....#', '....#', '...##', '.###.']
+    depthm1 = ['#..##', '...##', '.....', '...#.', '.####']
+    depth0  = ['.#...', '.#.##', '.#...', '.....', '.....']
+    depth1  = ['.##..', '#..##', '....#', '##.##', '#####']
+    depth2  = ['###..', '##.#.', '#....', '.#.##', '#.#..']
+    depth3  = ['..###', '.....', '#....', '#....', '#...#']
+    depth4  = ['.###.', '#..#.', '#....', '##.#.', '.....']
+    depth5  = ['####.', '#..#.', '#..#.', '####.', '.....']
+  end
+end
+
+#===========================================================
+class NilClass # Monkeypatch
+  #=========================================================
+  def cell(x,y)
+    0
+  end
 end
 
 #===========================================================
 class RecursiveSurface < Surface
+  #=========================================================
   def connect(down, up)
     @down = down
     @up = up
   end
 
   #=========================================================
-  def _adjacent_bugs(grid, x, y)
-    puts "_adjacent_bugs(grid, #{x}, #{y}) / #{@down} / #{@up}"
-    adjacent_count = 0
+  def cell(x, y)
+    @grid[y][x]
+  end
 
-    # TODO
-    # adjacent_count += 1 if (y > 0) && !grid[y - 1][x].zero?
-    # adjacent_count += 1 if (y < 4) && !grid[y + 1][x].zero?
-    # adjacent_count += 1 if (x > 0) && !grid[y][x - 1].zero?
-    # adjacent_count += 1 if (x < 4) && !grid[y][x + 1].zero?
-    adjacent_count
+  #=========================================================
+  def _adjacent_bugs(x, y)
+
+    # Scan in basic order N E S W
+    case [x, y]
+    # Row 0
+    when [0,0]
+      @up.cell(2, 1) + cell(x + 1, y) + cell(x, y + 1) + @up.cell(1, 2)
+    when [1,0], [2,0], [3,0]
+      @up.cell(2,1) + cell(x + 1, y) + cell(x, y + 1) + cell(x - 1, y)
+    when [4,0]
+      @up.cell(2,1) + @up.cell(3, 2) + cell(x, y + 1) + cell(x - 1, y)
+
+    # Row 1
+    when [0,1]
+      cell(x, y - 1) + cell(x + 1, y) + cell(x, y + 1) + @up.cell(1, 2)
+    when [1,1], [3,1]
+      cell(x, y - 1) + cell(x + 1, y) + cell(x, y + 1) + cell(x - 1, y)
+    when [4,1]
+      cell(x, y - 1) + @up.cell(3, 2) + cell(x, y + 1) + cell(x - 1, y)
+    when [2,1]
+      cell(x, y - 1) + @up.cell(3, 2) + (@down.cell(0,0) + @down.cell(1,0) + @down.cell(2,0) + @down.cell(3,0) + @down.cell(4,0)) + cell(x - 1, y)
+
+    # Row 2
+    when [0,2]
+      cell(x, y - 1) + cell(x + 1, y) + cell(x, y + 1) + @up.cell(1, 2)
+    when [1,2]
+      cell(x, y - 1) + (@down.cell(0,0) + @down.cell(0,1) + @down.cell(0,2) + @down.cell(0,3) + @down.cell(0,4)) + cell(x, y + 1) + cell(x - 1, y)
+    when [2,2]
+      0 # Magic centre
+    when [3,2]
+      cell(x, y - 1) + cell(x + 1, y) + (@down.cell(4,0) + @down.cell(4,1) + @down.cell(4,2) + @down.cell(4,3) + @down.cell(4,4)) + cell(x - 1, y)
+    when [4,2]
+      cell(x, y - 1) + @up.cell(3, 2) + cell(x, y + 1) + cell(x - 1, y)
+
+    # Row 3
+    when [0,3]
+      cell(x, y - 1) + cell(x + 1, y) + cell(x, y + 1) + @up.cell(1, 2)
+    when [1,3], [3,3]
+      cell(x, y - 1) + cell(x + 1, y) + cell(x, y + 1) + cell(x - 1, y)
+    when [4,3]
+      cell(x, y - 1) + @up.cell(3, 2) + cell(x, y + 1) + cell(x - 1, y)
+    when [2,3]
+      (@down.cell(0,4) + @down.cell(1,4) + @down.cell(2,4) + @down.cell(3,4) + @down.cell(4,4)) + @up.cell(3, 2) + cell(x, y + 1) + cell(x - 1, y)
+
+    # Row 4
+    when [0,4]
+      cell(x, y - 1) + cell(x + 1, y) + @up.cell(2, 3) + @up.cell(1, 2)
+    when [1,4], [2,4], [3,4]
+      cell(x, y - 1) + cell(x + 1, y) + @up.cell(2, 3) + cell(x - 1, y)
+    when [4,4]
+      cell(x, y - 1) + @up.cell(3, 2) + @up.cell(2, 3) + cell(x - 1, y)
+
+    else
+      0
+    end
   end
 end
